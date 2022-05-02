a2e9c270cdb97b40fcd5233feddb2225d12d9397
axel <axel.siriez@gmail.com>
2022-05-02 10:32:38 +0200

    hello world

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './hello';
+import App from './hello';
 
-function App() {
+const app = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="App">
+      <Hello />
     </div>
-  );
+  )
 }
+
+let n = 0
+
+function render() {
+  React.createElement('span', {}, n)
+  window.setInterval(() => {
+    n++
+    render()
+  }, 1000)
+  return n
+}
+
+render()
+
+
 
 export default App;
