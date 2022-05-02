73b25ab3eb873af91d1d4fa441fde1c14d591a4b
axel <axel.siriez@gmail.com>
2022-05-02 15:47:03 +0200

    font:fix

--- woodstock/src/header.jsx
+++ woodstock/src/header.jsx
@@ -1,14 +1,19 @@
 import logo from './wood-logo.svg';
 import header1 from './wood-left.svg';
 import header2 from './wood-right.svg';
+import woodstock from './WOOD-STOCK.svg';
 
 const Header = () => {
   return (
     <header className="App-header">
+
       <div className='header-back'>
         <img src={header2} className='header2'></img>
         <img src={header1} className='header1'></img>
         <nav className="App-nav">
+          <div className='header-middle'>
+            <img src={woodstock} alt="wood header" className='woodstock-header' />
+          </div>
           <a href="#">
             <div className='burger-menu'>
               <span className='bar'></span>
@@ -16,9 +21,8 @@ const Header = () => {
               <span className='bar'></span>
             </div>
           </a>
-          <a href="#"><img src={logo} alt="wood header" />
-          </a>
           <span className="nav-link">
+            <a href="#"><img src={logo} alt="wood header" /></a>
             <a href="#">About</a>
             <a href="#">Furnitures</a>
             <a href="#">Shop</a>
