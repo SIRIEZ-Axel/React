852c76db9f6083cd213e3639ee4a57422a9181df
axel <axel.siriez@gmail.com>
2022-05-05 13:43:08 +0200

    la maison de la jungle:fix

--- la-maison-jungle/src/App.js
+++ la-maison-jungle/src/App.js
@@ -1,17 +1,24 @@
-import Banner from './assets/js/Banner.jsx';
-import Cart from './assets/js/cart.jsx';
-import Desc from './assets/js/description.jsx';
-import Shoppinglist from './assets/js/ShoppingList.jsx';
+import Banner from './assets/js/Banner'
+import logo from './assets/img/leaf.png'
+import Cart from './assets/js/cart'
+import ShoppingList from './assets/js/ShoppingList'
+import './assets/style/layout.css'
+import Footer from './assets/js/footer'
 
 function App() {
-  return (
-    <div>
-      <Banner />
-      <Cart />
-      <Desc />
-      <Shoppinglist />
-    </div>
-  );
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			<div className='lmj-layout-inner'>
+				<Cart />
+				<ShoppingList />
+			</div>
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ la-maison-jungle/src/assets/datas/planList.js
@@ -0,0 +1,87 @@
+import monstera from '../img/monstera.jpg'
+import lyrata from '../img/lyrata.jpg'
+import pothos from '../img/pothos.jpg'
+import succulent from '../img/succulent.jpg'
+import olivier from '../img/olivier.jpg'
+import basil from '../img/basil.jpg'
+import mint from '../img/mint.jpg'
+import calathea from '../img/calathea.jpg'
+import cactus from '../img/cactus.jpg'
+
+export const plantList = [
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed',
+		light: 2,
+		water: 3,
+		cover: monstera
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+		light: 3,
+		water: 1,
+		cover: lyrata
+	},
+
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+		light: 1,
+		water: 2,
+		cover: pothos
+	},
+	{
+		name: 'calathea',
+		category: 'classique',
+		id: '4kk',
+		light: 2,
+		water: 3,
+		cover: calathea
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+		light: 3,
+		water: 1,
+		cover: olivier
+	},
+
+	{
+		name: 'cactus',
+		category: 'plante grasse',
+		id: '8fp',
+		light: 2,
+		water: 1,
+		cover: cactus
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+		light: 2,
+		water: 3,
+		cover: basil
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn',
+		light: 2,
+		water: 1,
+		cover: succulent
+	},
+
+	{
+		name: 'menthe',
+		category: 'extérieur',
+		id: '6uo',
+		light: 2,
+		water: 2,
+		cover: mint
+	}
+]
